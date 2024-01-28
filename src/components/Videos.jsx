0c6ad0a0f47079d335b9d7edb004dcffc9432424
Michael Quesada <mjquesada45@gmail.com>
2024-01-28 17:37:32 -0500

    Added RapidAPI to source youtube videos

---
+++ src/components/Videos.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Videos = ({ videos }) => {
+  console.log(videos)
+  return (
+    <div>Vidoes</div>
+  )
+}
+
+export default Videos
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com/search";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}?${url}`, options);
+      return data;
+    } catch (error) {
+      console.error("Error fetching data from the API:", error);
+      throw error; 
+    }
+  };
