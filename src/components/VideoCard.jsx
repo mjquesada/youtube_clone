c243283bce47077d8cdfa8cd01004fedd23c0e04
Michael Quesada <mjquesada45@gmail.com>
2024-01-30 20:16:54 -0500

    Added to the VideoCard.

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,14 +1,23 @@
-import { Link } from 'react-router-dom';
-import { Typography, Card, CardContent, CardMedia } from '@mui/material'
-import { CheckCircle } from '@mui/icons-material'
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
 
-import { demoThumbnailUrl, demoVideoUrl, demoChannelUrl, demoChannelTitle, demoVideoTitle } from '../utils/constants';
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoChannelUrl,
+  demoChannelTitle,
+  demoVideoTitle,
+} from "../utils/constants";
 
 const VideoCard = ({ video }) => {
-    console.log(video)
-  return (
-    <div>VideoCard</div>
-  )
-}
 
-export default VideoCard
+    console.log('Video ID: ', video)
+    //console.log('Snippet: ', snippet)
+  return (
+    <Card>
+    </Card>
+  );
+};
+
+export default VideoCard;
